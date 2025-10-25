c652142009087c560fb230aca98513eb5c8b6b00
martin mugo <bralva.co15@gmail.com>
2025-10-25 11:12:11 +0300

    fixed a bug

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,6 +1,6 @@
 const quotevalue = document.getElementById('quoteDisplay');
 
-quotes = [
+const quotes = [
     {quote: "Be yourself; everyone else is already taken", category: "selflove"},
     {quote:"The most difficult thing is the decision to act, the rest is merely tenacity.", category:"inspiration"},
     {quote:"so many books, so little time", category:"wisdom"}

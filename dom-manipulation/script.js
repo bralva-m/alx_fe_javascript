77f256c1cc4875817a1a786e864967132a6c641e
martin mugo <bralva.co15@gmail.com>
2025-10-25 11:06:26 +0300

    added a form to get user quotes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -5,11 +5,33 @@ quotes = [
     {quote:"The most difficult thing is the decision to act, the rest is merely tenacity.", category:"inspiration"},
     {quote:"so many books, so little time", category:"wisdom"}
 ]
-function newquote() {
+function showRandomQuote() {
     const randomquote = quotes[Math.floor(Math.random()* quotes.length)];
-    quotevalue.textContent = `"${randomquote.quote}" - ${randomquote.category}`;;
+    quotevalue.innerHTML = `"${randomquote.quote}" - ${randomquote.category}`;
 }
 
-button = document.getElementById('newQuote');
+function addQuote () {
+    let newQuoteText = document.getElementById('newQuoteText');
+const quoteText = newQuoteText.value.trim();
 
-button.addEventListener("click", newquote);
+let newQuoteCategory = document.getElementById('newQuoteCategory');
+const quoteCategory = newQuoteCategory.value.trim();
+
+    if (quoteText !== "" && quoteCategory !== "") {
+            quotes.push ({quote: quoteText, category: quoteCategory});
+
+            console.log(quotes);
+            showRandomQuote();
+          
+}
+
+}
+
+
+const button = document.getElementById('newQuote');
+
+
+button.addEventListener("click", function() {
+    addQuote();
+    showRandomQuote();
+});
